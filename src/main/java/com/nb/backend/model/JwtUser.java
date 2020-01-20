d08f35d01a0058160f5052d133b2fceb9e0afd3d
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-20 18:57:36 -0500

    JwtUser y JwtUserDetails

---
+++ src/main/java/com/nb/backend/model/JwtUser.java
@@ -0,0 +1,23 @@
+package com.nb.backend.model;
+
+public class JwtUser {
+
+    private String username;
+    private long id;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/nb/backend/model/JwtUserDetails.java
@@ -0,0 +1,54 @@
+package com.nb.backend.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class JwtUserDetails implements UserDetails {
+
+    private String username;
+    private Long id;
+    private String token;
+
+    public JwtUserDetails(String username, Long id, String token) {
+        this.username = username;
+        this.id = id;
+        this.token = token;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
