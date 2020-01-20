ed7562c59b306089f6e02ebca870f7edf981941c
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-20 18:43:01 -0500

    Contantes

---
+++ src/main/java/com/nb/backend/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.nb.backend.constants;
+
+public class Constants {
+
+    public final static String SECRET_KEY = "nb";
+    public final static String USER_ID = "user_id";
+    public final static String AUTHORIZATION_HEADER = "Authorization";
+    public final static String BEARER_TOKEN = "Bearer ";
+}
