e8e94cd302966b1ca1663cf180665673518ea919
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-22 10:35:05 -0500

    Completando JwtSecurityConfig

--- src/main/java/com/nb/backend/config/JwtSecurityConfig.java
+++ src/main/java/com/nb/backend/config/JwtSecurityConfig.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -40,5 +42,15 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
         tokenFilter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
 
         return tokenFilter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and()
+                .csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
