a3bbd572d3f461b724c951d8e146dbbabf7164b3
namelessbliss <anonimo2911.2012@gmail.com>
2020-01-22 11:05:24 -0500

    Task Service Impl

---
+++ src/main/java/com/nb/backend/service/TaskServiceImpl.java
@@ -0,0 +1,43 @@
+package com.nb.backend.service;
+
+import com.nb.backend.dao.ITaskDao;
+import com.nb.backend.entity.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public class TaskServiceImpl implements ITaskService {
+
+    @Autowired
+    private ITaskDao taskDao;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Task> findAll() {
+        return (List<Task>) taskDao.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void saveTask(Task task) {
+        taskDao.save(task);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Task> getTaskUser(Long id) {
+        return (List<Task>) taskDao.findByUserId(id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Task findByIdSQL(Long id) {
+        return taskDao.findByIdSQL(id);
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        taskDao.deleteById(id);
+    }
+}
